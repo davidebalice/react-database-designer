5a3f6fd08d94fc48adf8775d5baf7e61299ec622
davidebalice <davide.balice@gmail.com>
2024-08-20 16:11:54 +0200

    update

--- src/pages/designer/Modal.js
+++ src/pages/designer/Modal.js
@@ -1,12 +1,33 @@
+import {
+  faCirclePlus,
+  faClose,
+  faPenToSquare,
+  faSave,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Modal = ({ show, handleClose, selectedTable }) => {
   const [tableData, setTableData] = useState(null);
+  const [modTitle, setModTitle] = useState(false);
+  const [modField, setModField] = useState("");
   const [loading, setLoading] = useState(false);
   const [isSaving, setIsSaving] = useState(false);
 
   const getAuthToken = () => localStorage.getItem("authToken");
+  const fieldTypes = [
+    "int",
+    "tinyint",
+    "bigint",
+    "float",
+    "boolean",
+    "double",
+    "varchar",
+    "text",
+    "datetime",
+    "date",
+  ];
 
   useEffect(() => {
     if (show) {
@@ -51,28 +72,61 @@ const Modal = ({ show, handleClose, selectedTable }) => {
     }
   };
 
-  const handleSaveChanges = async () => {
+  const handleSave = async () => {
     console.log("Sending updated tableData:", tableData);
     setIsSaving(true);
+
+    const sanitizedTableData = {
+      ...tableData,
+      fields: tableData.fields.map((field) => {
+        if (typeof field.id === "string" && field.id.startsWith("temp-")) {
+          const { id, ...rest } = field;
+          return rest;
+        }
+        return field;
+      }),
+    };
+
     try {
       const token = getAuthToken();
       await axios.post(
         `${process.env.REACT_APP_API_BASE_URL}edit/table/${tableData.id}`,
-        tableData,
+        sanitizedTableData,
         {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         }
       );
-      alert("Changes saved successfully!");
       handleClose();
     } catch (error) {
       console.error("Error saving changes:", error);
-      alert("Failed to save changes.");
     } finally {
       setIsSaving(false);
     }
+  };
+
+  const handleNewField = () => {
+    const newField = {
+      id: `temp-${Date.now()}`,
+      name: "Field name",
+      field_type: "varchar",
+    };
+
+    setTableData((prevData) => ({
+      ...prevData,
+      fields: [...prevData.fields, newField],
+    }));
+  };
+
+  const handleFieldChange = (e, fieldId) => {
+    const { name, value } = e.target;
+    setTableData((prevData) => ({
+      ...prevData,
+      fields: prevData.fields.map((field) =>
+        field.id === fieldId ? { ...field, [name]: value } : field
+      ),
+    }));
   };
 
   if (!show) return null;
@@ -81,51 +135,113 @@ const Modal = ({ show, handleClose, selectedTable }) => {
     <div className="modal-overlay" onClick={handleClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <div className="modal-header">
-          <input
-            type="text"
-            name="name"
-            value={tableData?.name || ""}
-            onChange={handleInputChange}
-            placeholder="Table Name"
-          />
-          <button onClick={handleClose} className="close-btn">
-            &times;
-          </button>
+          {modTitle ? (
+            <div className="flex-row">
+              <input
+                type="text"
+                name="name"
+                value={tableData?.name || ""}
+                onChange={handleInputChange}
+                placeholder="Table name"
+                className="modal-title-input"
+              />
+              <div onClick={() => setModTitle(!modTitle)}>
+                <FontAwesomeIcon icon={faClose} className="modal-icon" />
+              </div>
+            </div>
+          ) : (
+            <>
+              <div className="flex-row">
+                <span>{tableData?.name || ""}</span>
+                <div onClick={() => setModTitle(!modTitle)}>
+                  <FontAwesomeIcon
+                    icon={faPenToSquare}
+                    className="modal-icon"
+                  />
+                </div>
+              </div>
+            </>
+          )}
+          <div className="modal-button-container">
+            <button onClick={handleNewField} className="modal-button">
+              <FontAwesomeIcon icon={faCirclePlus} className="modal-icon" />
+              New field
+            </button>
+            <button
+              onClick={handleSave}
+              disabled={isSaving}
+              className="modal-button"
+            >
+              <FontAwesomeIcon icon={faSave} className="modal-icon" />
+              {isSaving ? "Saving..." : "Save"}
+            </button>
+            <button onClick={handleClose} className="modal-button">
+              <FontAwesomeIcon icon={faClose} className="modal-icon" />
+              Close
+            </button>
+          </div>
         </div>
         <div className="modal-body">
           {loading ? (
             <p>Loading...</p>
           ) : tableData ? (
             <div>
-              <ul className="field-row">
+              <ul className="w-100">
                 {tableData.fields.map((field) => (
-                  <li key={field.id}>
-                    <input
-                      type="text"
-                      name="name"
-                      value={field.name}
-                      onChange={(e) => handleInputChange(e, field.id)}
-                      placeholder="Field Name"
-                    />
-                    <input
-                      type="text"
-                      name="field_type"
-                      value={field.field_type}
-                      onChange={(e) => handleInputChange(e, field.id)}
-                      placeholder="Field Type"
-                    />
+                  <li key={field.id} className="field-row">
+                    {modField === field.id ? (
+                      <>
+                        <div>key</div>
+                        <div>
+                          <input
+                            type="text"
+                            name="name"
+                            value={field.name}
+                            onChange={(e) => handleInputChange(e, field.id)}
+                            placeholder="Field Name"
+                          />
+                        </div>
+                        <div>
+                          <select
+                            name="field_type"
+                            value={field.field_type}
+                            onChange={(e) => handleFieldChange(e, field.id)}
+                          >
+                            {fieldTypes.map((type) => (
+                              <option key={type} value={type}>
+                                {type}
+                              </option>
+                            ))}
+                          </select>
+                        </div>
+                        <div onClick={() => setModField("")}>
+                          <FontAwesomeIcon
+                            icon={faClose}
+                            className="modal-icon2"
+                          />
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <div>key</div>
+                        <div>{field.name}</div>
+                        <div>{field.field_type}</div>
+
+                        <div onClick={() => setModField(field.id)}>
+                          <FontAwesomeIcon
+                            icon={faPenToSquare}
+                            className="modal-icon2"
+                          />
+                        </div>
+                      </>
+                    )}
                   </li>
                 ))}
               </ul>
             </div>
           ) : (
-            <p>No data available</p>
+            <p>Fileds not found</p>
           )}
-        </div>
-        <div className="modal-footer">
-          <button onClick={handleSaveChanges} disabled={isSaving}>
-            {isSaving ? "Saving..." : "Save Changes"}
-          </button>
         </div>
       </div>
     </div>
